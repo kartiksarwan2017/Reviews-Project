1f38e8cf18fd88c439cf178bcfeb28e8ff6d5f17
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-10-15 01:00:23 +0530

    Reviews project using Vanilla JS

---
+++ index.js
@@ -0,0 +1,121 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      img: 'https://www.course-api.com/images/people/person-2.jpeg',
+      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+
+// select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+const reviewCard = document.querySelector('.review');
+
+// set starting item
+let currentItem = 0;
+const colors = ["#98fb98", "#ecffe5", "#c9a0dc", "#fdeeee", "#fceee9", "#fcead7", "#98fb98"];
+
+
+/* once the document loads we want to access the review object at index 0 
+
+and display it as a review intially.
+*/
+
+// load initial item
+/* DOMContentLoaded is the event listener which gets activated when the web page gets loaded
+
+The DOMContentLoaded event fires when the initial HTML
+document has been completely loaded and parsed.
+
+*/
+window.addEventListener('DOMContentLoaded', function(){
+    showPerson(currentItem);
+});
+
+
+// show person based on item
+function showPerson(person){
+
+    // Accessing the first item from the reviews array
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+    reviewCard.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+
+    let selectedColor = "";
+    let selectedColorIndex = Math.floor(Math.random() * colors.length);
+
+    selectedColor = colors[selectedColorIndex];
+
+    return selectedColor;
+}
+
+// show next person
+nextBtn.addEventListener('click', function(){
+
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+
+    showPerson(currentItem);
+
+});
+
+
+// show prev person
+prevBtn.addEventListener('click', function() {
+
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length-1;
+    }
+
+    showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener('click', () => {
+    
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+
+
+    
+});
+
